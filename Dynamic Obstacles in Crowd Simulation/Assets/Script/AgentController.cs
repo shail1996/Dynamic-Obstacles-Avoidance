12bf965bf3afc28f5fb9104b40f207a6c66a6d15
Shail <shail1996@gmail.com>
2020-06-20 17:14:32 +0530

    Initial commit

---
+++ Dynamic Obstacles in Crowd Simulation/Assets/Script/AgentController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+    public Transform destination;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(destination.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Dynamic Obstacles in Crowd Simulation/Assets/Script/DynamicObstacles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicObstacles : MonoBehaviour
+{
+    public Transform destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position -= transform.forward * Time.deltaTime * 2;
+        transform.position = Vector3.MoveTowards(transform.position, destination.position, Time.deltaTime * 2);
+    }
+}
